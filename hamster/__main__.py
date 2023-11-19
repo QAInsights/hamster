202c3a67c5cb2b5cc4efa4bd0fd640fa1f079060
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2023-11-19 12:13:50 -0500

    Add: GitHub Action
    Signed-off-by: NaveenKumar Namachivayam <catch.nkn@gmail.com>

--- hamster/__main__.py
+++ hamster/__main__.py
@@ -1,10 +1,21 @@
-import config, menu, utils
+import menu
+import os
 
 
 def main():
     """
     Entry point of the Hamster - menu bar application.
     """
+    # Get the path to the user's home directory
+    home_dir = os.path.expanduser("~")
+
+    # Create a new directory within the home directory
+    app_dir = os.path.join(home_dir, "HamsterApp")
+    os.makedirs(app_dir, exist_ok=True)
+
+    # Specify the path to the properties file within the new directory
+    properties_file = os.path.join(app_dir, "app.properties")
+
     menu_builder = menu.DynamicMenuApp("JMeter")
     menu_builder.run()
 
