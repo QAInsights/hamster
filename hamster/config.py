074f5b3f10524dab7255533036f442e9308c751c
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2023-11-19 12:24:36 -0500

    Add: GitHub Action
    Signed-off-by: NaveenKumar Namachivayam <catch.nkn@gmail.com>

--- hamster/config.py
+++ hamster/config.py
@@ -3,12 +3,26 @@ import os
 import sys
 import getpass
 import re
+import shutil
+
+
+# Get the path to the user's home directory
+home_dir = os.path.expanduser("~")
+
+# Define the source path (current directory)
+source_path = os.path.join(os.path.dirname(sys.argv[0]), 'app.properties')
+
+# Define the destination path (user's home directory)
+destination_path = os.path.join(home_dir, 'app.properties')
+
+# Use shutil to copy the file
+shutil.copy(source_path, destination_path)
 
 config_parser = configparser.ConfigParser()
-properties_file_path = os.path.join(os.path.dirname(sys.argv[0]), 'app.properties')
+properties_file_path = os.path.join(home_dir, 'app.properties')
 config_parser.read(properties_file_path)
 
-icon_path=os.path.join(os.path.dirname(sys.argv[0]), 'img/hamster.png')
+icon_path = os.path.join(os.path.dirname(sys.argv[0]), 'img/hamster.png')
 username = getpass.getuser()
 pattern = re.compile("recent_file_.*")
 
