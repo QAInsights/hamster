e2a16a0cf947f3be098000af42f0871b23beabd6
Leela Prasad <47483946+leelaprasadv@users.noreply.github.com>
2023-11-21 17:20:57 +0530

    Add Recent test plan menu and refactor code

--- windows/main.py
+++ windows/main.py
@@ -1,45 +1,46 @@
 import shutil
-import sys
 import configparser
 import re
-import pystray
+import time
 import winreg
 import subprocess
 import os
 
-import tkinter as tk
-from tkinter import simpledialog
-
+from collections import OrderedDict
+from tkinter import filedialog, messagebox
 from PIL import Image
-from pystray import MenuItem as item
+from pystray import MenuItem, Menu, Icon
+from contextlib import suppress
 
+# Declarations
 jmeter_recent_files_pattern = re.compile("recent_file_.*")
-jmeter_home = r'C:\Tools\apache-jmeter-5.6.2\bin\jmeter.bat'
+
 app_title = 'Hamster'
 app_title_emoji = '🐹 Hamster'
 app_caption = 'Instantly Launch JMeter Test Plans'
 app_caption_emoji = 'Instantly Launch JMeter Test Plans 🚀'
-
-app_properties_template = "hamster_app.ini"
+app_properties_template = "hamster_app.properties"
+win_app_properties = app_properties_template.replace(".properties", ".ini")
 home_dir = os.path.expanduser("~")
 
 config_parser = configparser.ConfigParser()
 properties_folder_path = os.path.join(home_dir, 'Appdata', 'Local', app_title)
-properties_file_path = os.path.join(home_dir, 'Appdata', 'Local', app_title, app_properties_template)
+properties_file_path = os.path.join(home_dir, 'Appdata', 'Local', app_title, win_app_properties)
+
+menu_items_dict = OrderedDict()
 
 
 def create_app_data_dir():
     # create app data dir in user home\appdata\local
-
     if not os.path.exists(properties_folder_path):
         os.makedirs(properties_folder_path)
         # check for ini file inside the app data dir
-        if not os.path.isfile(os.path.join(properties_folder_path, app_properties_template)):
-            # copy the hamster_app.ini file to the app data dir
-            print(f'Copying {app_properties_template} to {properties_folder_path}')
-            source_file = os.path.join(os.getcwd(), app_properties_template)
-            destination_file = os.path.join(properties_folder_path, app_properties_template)
-            shutil.copyfile(source_file, destination_file)
+    if not os.path.exists(os.path.join(properties_folder_path, win_app_properties)):
+        # copy the hamster_app.properties file to the app data dir
+        print(f'Copying {app_properties_template} to {properties_folder_path}')
+        source_file = os.path.join(os.getcwd(), app_properties_template)
+        destination_file = os.path.join(properties_folder_path, win_app_properties)
+        shutil.copyfile(source_file, destination_file)
 
 
 def update_properties(properties):
@@ -59,84 +60,114 @@ def read_properties():
         return value
 
 
-def get_user_input():
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
-    properties = simpledialog.askstring(title=f"{app_title} - Configure JMETER_HOME", prompt=r"Please enter JMETER_HOME"
-                                                                                             r" path e.g."
-                                                                                             r"C:\Tools\apache-jmeter"
-                                                                                             r"-5.6.2",
-                                        initialvalue=read_properties())
-    update_properties({'HOME': str(properties).strip()})
-
-
 def get_recent_test_plans():
     recent_test_plans = []
-    path = r'Software\JavaSoft\Prefs\org\apache\jmeter\gui\action'
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path)
-    i = 0
-    while True:
-        try:
-            value_name, value_data, value_type = winreg.EnumValue(key, i)
-            if re.match(jmeter_recent_files_pattern, value_name):
-                # print(f'Name: {value_name}, Data: {value_data}, Type: {value_type}')
-                clean_jmeter_path = value_data.replace('///', '\\').replace('//', '\\').replace('/', '')
-                # print("Cleaned " + clean_jmeter_path)
-                recent_test_plans.append(clean_jmeter_path)
-            i += 1
-        except OSError:
-            break
-    winreg.CloseKey(key)
+    key_path = r'Software\JavaSoft\Prefs\org\apache\jmeter\gui\action'
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path) as key:
+            index = 0
+            while True:
+                try:
+                    value_name, value_data, _ = winreg.EnumValue(key, index)
+                    if re.match(jmeter_recent_files_pattern, value_name):
+                        clean_jmeter_path = value_data.replace('///', '\\').replace('//', '\\').replace('/', '')
+                        recent_test_plans.append(clean_jmeter_path)
+                    index += 1
+                except OSError as e:
+                    with suppress(OSError):
+                        winreg.CloseKey(key)
+                    if e.errno == 259:  # No more data is available
+                        break
+                    break
+    except FileNotFoundError:
+        print(f"Error: Registry key '{key_path}' not found.")
+    except Exception as e:
+        print(f"Error: {e}")
+
     return recent_test_plans
 
 
 def launch_test_plan(plan):
     # launch jmeter with the test plan
-    jmeter_args = '-t'
+    jmeter_plan = f'-t {plan}'
     config_parser.read(properties_file_path)
     jmeter_home = config_parser['JMETER']['HOME']
     jmeter_path = os.path.join(jmeter_home, 'bin', 'jmeter.bat')
+    jmeter_logs = f'-j {jmeter_home}\jmeter.log'
     print(f'Launching {jmeter_path}')
     if plan is None:
         subprocess.Popen([jmeter_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     else:
-        subprocess.Popen([jmeter_path, jmeter_args, plan], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.Popen([jmeter_path, jmeter_logs, plan], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return
 
 
-def on_click(icon, menu_item):
-    if str(menu_item).__contains__('Just JMeter'):
-        launch_test_plan(plan=None)
+def action_launch_jmeter(icon, menu_item):
+    launch_test_plan(plan=None)
 
-    if str(menu_item).__contains__('.jmx'):
-        launch_test_plan(str(menu_item))
 
-    if str(menu_item).__contains__('Configure'):
-        print('Configure JMETER_HOME')
-        get_user_input()
+def action_recent_test_plan(icon, menu_item):
+    launch_test_plan(menu_item.text)
+    refresh_test_plans(icon)
 
-    if str(menu_item) == 'Quit':
-        icon.stop()
+
+def action_refresh(icon, _):
+    refresh_test_plans(icon, 0)
+
+
+def action_view_config():
+    __jmeter_home = read_properties()
+    messagebox.showinfo("Hamster Config", f"JMeter Home: {__jmeter_home}")
+
+
+def action_edit_config():
+    _default_jmeter_home = read_properties()
+    _jmeter_home = filedialog.askdirectory(initialdir=_default_jmeter_home, mustexist=True,
+                                           title=f"{app_title} - Configure JMETER_HOME")
+    if _jmeter_home:
+        update_properties({'HOME': str(_jmeter_home).strip()})
+
+
+def action_quit(icon, _):
+    icon.stop()
+
+
+def refresh_test_plans(icon, delay=5):
+    global menu_items_dict
+    time.sleep(delay)
+    updated_test_plans = get_recent_test_plans()
+    refreshed_test_plans_menu_items = []
+    for idx, plan in enumerate(updated_test_plans, start=1):
+        refreshed_test_plans_menu_items.append(MenuItem(plan, action_recent_test_plan))
+    menu_items_dict["Recent Test Plans"] = MenuItem('Recent Test Plans', Menu(*refreshed_test_plans_menu_items))
+    icon.menu = Menu(*menu_items_dict.values())
 
 
 def main():
+    global menu_items_dict
     image = Image.open("hamster.png")
+    recent_test_plans_menu_items = []
 
-    # Create the menu items
-    menu_items = [item('🚀 Just JMeter', on_click), pystray.Menu.SEPARATOR]
     recent_test_plans = get_recent_test_plans()
     for idx, plan in enumerate(recent_test_plans, start=1):
-        menu_items.append(item(plan, on_click))
+        recent_test_plans_menu_items.append(MenuItem(plan, action_recent_test_plan))
 
-    menu_items.append(pystray.Menu.SEPARATOR)
-    menu_items.append(item('Configure JMETER_HOME', on_click))
-    menu_items.append(item('Quit', on_click))
+    menu_items_dict.update({
+        "Launch JMeter": MenuItem('🚀 Launch JMeter', action_launch_jmeter),
+        "Recent Test Plans": MenuItem('Recent Test Plans', Menu(*recent_test_plans_menu_items)),
+        "Seperator01": Menu.SEPARATOR,
+        "View Config": MenuItem('View Config', action_view_config),
+        "Configure JMETER_HOME": MenuItem('Configure JMETER_HOME', action_edit_config),
+        "Seperator02": Menu.SEPARATOR,
+        "Refresh": MenuItem('Refresh', action_refresh),
+        "Quit": MenuItem('Quit', action_quit)
+    })
 
     # Create the menu with the menu items
-    menu = pystray.Menu(*menu_items)
+    menu = Menu(*menu_items_dict.values())
 
     # Create the icon with the menu
-    icon = pystray.Icon("Hamster", image, f"{app_title} - {app_caption}", menu)
+    icon = Icon("Hamster", image, f"{app_title} - {app_caption}", menu)
     icon.run()
 
 
