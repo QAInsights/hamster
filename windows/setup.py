1f2c336d7dd697135902798e0ba319768cb846ac
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2023-11-21 18:57:02 -0500

    Fix:MSI and Exe
    Signed-off-by: NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>

--- windows/setup.py
+++ windows/setup.py
@@ -1,33 +1,16 @@
-import PyInstaller.__main__
-
 import sys
-import os
+from cx_Freeze import setup, Executable
 
-if getattr(sys, 'frozen', False):
-    # Running in a bundle
-    base_path = sys._MEIPASS
-else:
-    # Running in normal Python environment
-    base_path = os.path.abspath(".")
+build_exe_options = {
+    "include_files": ["hamster.png"]
+}
 
-image_path = os.path.join(base_path, "hamster.png")
-print(image_path)
+base = "Win32GUI" if sys.platform == "win32" else None
 
-# Get the directory of this script file
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-# Construct the path to hamster.png relative to the script directory
-hamster_path = os.path.join(script_dir, r'hamster.png')
-# Convert the path to a raw string
-hamster_path = r'{}'.format(hamster_path)
-
-# Print the path and the current working directory
-print("Path to hamster.png: ", hamster_path)
-print("Current working directory: ", os.getcwd())
-
-PyInstaller.__main__.run([
-    '--onefile',
-    '--windowed',
-    '--add-data={}:.'.format(hamster_path),
-    'main.py'
-])
+setup(
+    name="Hamster",
+    version="0.1",
+    description="Instantly Launch JMeter Test Plans 🚀",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=base)],
+)
