142971333987b533fc767c344c6914a616d14540
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2023-11-22 22:25:46 -0500

    Add: GH Workflow for Windows
    Signed-off-by: NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>

--- windows/setup.py
+++ windows/setup.py
@@ -1,16 +1,16 @@
 import sys
 from cx_Freeze import setup, Executable
-from windows import __VERSION__
+# from windows import __VERSION__
 
 build_exe_options = {
-    "include_files": ["hamster.png"]
+    "include_files": ["img\hamster.png"]
 }
 
 base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
     name="Hamster",
-    version=__VERSION__,
+    version="0.0.1",
     description="Instantly Launch JMeter Test Plans 🚀",
     options={"build_exe": build_exe_options},
     executables=[Executable("main.py", base=base)],
