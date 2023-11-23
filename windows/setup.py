d2ed727c3e5c3860fdb3f4f076381fe544080dac
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2023-11-23 11:24:22 -0500

    Test: Setup
    Signed-off-by: NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>

--- windows/setup.py
+++ windows/setup.py
@@ -2,6 +2,17 @@ import sys
 from cx_Freeze import setup, Executable
 # from windows import __VERSION__
 
+msi_data = {
+
+    "ProgId": [
+        ("Prog.Id", None, None, "This is a description", None, None),
+    ],
+}
+
+bdist_msi_options = {
+    "data": msi_data,
+}
+
 build_exe_options = {
     "include_files": ["hamster.png",
                       "config.py",
@@ -9,15 +20,25 @@ build_exe_options = {
                       "hamster_app.properties",
                       ],
 }
-
 base = "Win32GUI" if sys.platform == "win32" else None
+
+executables = [
+       Executable(
+            "main.py",
+            copyright="Copyright (C) 2023 cx_Freeze",
+            base=base,
+            shortcut_name="Hamster",
+        ),
+    ]
 
 setup(
     name="Hamster",
     version="0.0.1",
     description="Instantly Launch JMeter Test Plans 🚀",
-    options={"build_exe": build_exe_options},
-    executables=[
-        Executable("main.py", base=base)
-    ],
+    options={
+        "build_exe": build_exe_options,
+        "bdist_msi": bdist_msi_options,
+    },
+    executables=executables,
+    packages=["../windows"]
 )
